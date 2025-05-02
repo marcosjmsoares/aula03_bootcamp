596f0e32fb634f9ca88d8be41a1d436cd90fee9c
marcossoares-dev <55550244+marcossoares-dev@users.noreply.github.com>
2025-05-02 20:41:43 -0300

    exec 3

--- exercicios.py
+++ exercicios.py
@@ -33,6 +33,12 @@
 # como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
 # escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
 
+log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}
+
+if log['level'] == 'ERROR':
+    print(log['message'])
+
+
 ### Exercício 4: Validação de Dados de Entrada
 # Antes de processar os dados de usuários em um sistema de recomendação, 
 # você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha 
