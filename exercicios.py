543aec7c3aaa08ea3cf7e4afc767cac3e66a9126
marcossoares-dev <55550244+marcossoares-dev@users.noreply.github.com>
2025-05-04 16:51:23 -0300

    exerc 7

--- exercicios.py
+++ exercicios.py
@@ -99,6 +99,12 @@
 ### Exercício 7. Normalização de Dados
 # Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
 
+numeros = [10,20,30,40,50]
+minimo = min(numeros)
+maximo = max(numeros)
+normalizados = [(x - minimo) / (maximo - minimo) for x in numeros]
+
+print(normalizados)
 ### Exercício 8. Filtragem de Dados Faltantes
 # Objetivo:** Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando
 
